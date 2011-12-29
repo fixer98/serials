5c7f971444db88b760ae124c116407bde4842401
fixer98 <f1x3rffx@gmail.com>
2011-12-29 13:39:01 +0400

    1

---
+++ ser/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django import forms
+
+class Post(models.Model):
+    name_ru = models.CharField(max_length=25)
+    name_en = models.CharField(max_length=25)
+    description = models.TextField(max_length=1000)
+    seasons = models.CharField(max_length=40) #int
+    translate = models.CharField(max_length=50) #Novafilm;Lostfilm;NewStudios
+    imdb = models.CharField(max_length=100) #imbd id
+    kinopoisk = models.CharField(max_length=100) #API serial name
+    img = models.CharField(max_length=100) #url
+    date = models.DateField(auto_now_add=True)
+
+class PostForm(forms.Form):
+    name_ru = models.CharField(max_length=25)
+    name_en = models.CharField(max_length=25)
+    description = models.TextField(max_length=1000)
+    seasons = models.IntegerField(max_length=10)
+    imdb = models.CharField(max_length=100)
+    kinopoisk = models.CharField(max_length=100)
+    translate = models.CharField(max_length=50)
+    img = models.CharField(max_length=100)
